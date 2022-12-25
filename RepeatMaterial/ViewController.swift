ab2a2898c3df4717323056708fb94d9caae17d5e
Sergei <glushenko6314@icloud.com>
2022-12-25 23:11:42 +0300

    11

--- RepeatMaterial/ViewController.swift
+++ RepeatMaterial/ViewController.swift
@@ -6,11 +6,23 @@
 //
 
 import UIKit
+import EasyPeasy
 
 class ViewController: UIViewController {
+    private lazy var someView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
+        
+        view.addSubview(someView)
+        someView.easy.layout(
+            Height(100), Left(), Right(), CenterY().to(view, .centerY)
+        )
     }
 
 }
